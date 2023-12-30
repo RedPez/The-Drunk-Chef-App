ecc3ca5c395b442396d0e14a505b95636f7d9834
Fetie Hyuse <fethiyesilver@gmail.com>
2023-12-30 08:24:53 +0000

    creating HTML skeleton

--- asset/script.js
+++ asset/script.js
@@ -56,7 +56,47 @@ $(document).ready(function(){
 
 
 
+    // Sample data -
+const cocktails = [
+    {
+        name: 'Margarita',
+        image: 'margaritaImage.jpg',
+        ingredients: ['Tequila', 'Triple sec', 'Lime juice'],
+        recipe: 'Mix tequila, triple sec, and lime juice. Serve over ice.',        
+        video: '<iframe width="100%" height="100" src="https://www.youtube.com/embed/FE-VFXRZgLI?si=q2bJnVmW_vvhAleg" title="YouTube video player" frameborder="0" allowfullscreen; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>'
+    },
     
+];
+
+// Function to open the modal and populate it with data
+    function openModal(cocktailIndex) {
+    const modal = document.getElementById('cocktailModal');
+    const cocktail = cocktails[cocktailIndex];
+    document.getElementById('cocktailName').innerText = cocktail.name;
+    document.getElementById('cocktailImage').src = cocktail.image;
+
+    // Populate ingredients list
+    const ingredientsList = document.getElementById('ingredientsListContainer');
+    ingredientsList.innerHTML = '<h3><strong>Ingredients</strong></h3>';
+    for (const ingredient of cocktail.ingredients) {
+        const listItem = document.createElement('li');
+        listItem.innerText = ingredient;
+        ingredientsList.appendChild(listItem);
+    }
+
+    // Populate recipe text
+    const recipeText = document.getElementById('recipeTextContainer');
+    recipeText.innerHTML = `<h3><strong>Recipe</strong></h3>${cocktail.recipe}`;
+
+    // Populate video
+    const videoContainer = document.getElementById('video');
+    videoContainer.innerHTML = cocktail.video;
+    modal.style.display = 'block';
+}
+
+
+
+
 
 //     let youtubeUrl = "https://www.googleapis.com/youtube/v3/search?"
 //     let type = "type=video"
